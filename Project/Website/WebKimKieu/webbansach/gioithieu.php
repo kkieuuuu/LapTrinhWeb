0db5522ed74a325df8f8a096cba13051687066f1
kkieuuuu <nguyenthikimkieu31032002@gmail.com>
2023-05-11 19:10:37 +0700

    Add W

---
+++ Project/Website/WebKimKieu/webbansach/gioithieu.php
@@ -0,0 +1,25 @@
+﻿<div id="gioithieu">
+	<h3>GIỚI THIỆU</h3>
+	<p>
+		&nbsp &nbsp &nbsp &nbsp<br>
+		<br>
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<b>“WEBSITE BÁN SÁCH ONLINE” </b><br><br>Là trang web chuyên cung cấp các loại 
+		sách đến cho các bạn học sinh, sinh viên.<br><br> Giúp các bạn có thể tìm kiếm sách một cách nhanh chóng, dễ dàng và tiết kiệm thời gian hơn.
+<br><br> Tại đây, các bạn có thể thoải mái tìm kiếm các loại sách mà mình muốn một cách đơn giản và dễ dàng.
+<br><br>Chúc mọi người sẽ có những trải nghiệm tuyệt vời!!!
+<br><br><b>Nếu có bất kì thắc mắc nào đừng ngại liên hệ với chúng tôi nhé!</b><br><br>
+<br><b>CONTACT ME:</b>
+<br>PHONE: 0339317215
+<br>EMAIL: ntkkieuuuu@gmail.com
+<br>
+<img src="./img/tuine2.png" style="width:50%; height: 50%; position: relative; left: 250px; top:-70px" >
+<a href="https://www.facebook.com/kkieuuuu.313/"><img src= "./img/icon/facebook.png" width="10%" style="position: relative;top:-130px; left: -200px;" ></a>
+  <a href="https://www.instagram.com/ky2w_nee/"><img src= "./img/icon/instagram.png" width="10%" style="position: relative;top: -130px ;left: -170px;"></a>
+</p>
+<br>
+<br>
+ <br>
+ <br>&nbsp &nbsp &nbsp &nbsp<br>
+  </p>
+
+</div>
